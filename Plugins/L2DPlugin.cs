cbfa8fafcd99979004d967fc6b1e4570e4cb0043
Ahykal <lw203910@gmail.com>
2023-11-15 19:13:44 +0800

    Change live2D export command. Add spine export command. update plugin API.

--- Plugins/L2DPlugin.cs
+++ Plugins/L2DPlugin.cs
@@ -1,5 +1,7 @@
-﻿using AssetStudio.GUI;
+﻿using AssetStudio;
+using AssetStudio.GUI;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UnityLive2DExtractor;
@@ -8,70 +10,55 @@ namespace Plugins.Ahykal
 {
     public class L2DPlugin : IAssetPlugin
     {
-        public string RegisterMenuText => "Live2DTools";
-
-        public string Name => "Ahykal.Plugin";
-
+        public string Name => "Ahykal.Live2D";
+        public string RegisterMenuText => "Live2D";
         public RegisterMenuType RegisterMenuType => RegisterMenuType.Folder;
 
         public void Run(PluginConnection connection)
         {
-            this.SetCommand(new ToolStripMenuItem("ExportNormal")).Click += delegate
+            this.SetCommand(new ToolStripMenuItem("ExtractFromFolder")).Click += delegate
             {
                 connection.Mainform.OpenFolderDelegate(async o =>
                 {
                     var assetsManager = connection.Mainform.CreateAssetsManager();
                     await Task.Run(() => Live2DExtractor.Extract(assetsManager, o.Folder));
-                    await Console.Out.WriteLineAsync("exit");
+                    await Console.Out.WriteLineAsync("Done!");
                 });
             };
-            this.SetCommand(new ToolStripMenuItem("SaveMotionsByAnimators")).Click += delegate
+            this.SetCommand(new ToolStripSeparator());
+            var withPathIDCheck = new ToolStripMenuItem("ExportWithPathIDFolder") { CheckOnClick = true, Checked = true };
+            this.SetCommand(withPathIDCheck);
+
+            this.SetCommand(new ToolStripMenuItem("SaveAllByAnimators")).Click += delegate
             {
                 connection.Mainform.OpenFolderDelegate(o =>
                 {
-                    Util.SaveMotions(o.Folder, true);
-                });
-            };
-            this.SetCommand(new ToolStripMenuItem("SavePhysics")).Click += delegate
-            {
-                connection.Mainform.OpenFolderDelegate(o =>
-                {
-                    Util.SaveMP(Util.MPType.GetPhysicByClassName, o.Folder, true);
-                });
-            };
-            this.SetCommand(new ToolStripMenuItem("SavePhysics(2)")).Click += delegate
-            {
-                connection.Mainform.OpenFolderDelegate(o =>
-                {
-                    Util.SaveMP(Util.MPType.GetPhysicByClassName | Util.MPType.GetPhysicBySubRig, o.Folder, true);
-                });
-            };
-            this.SetCommand(new ToolStripMenuItem("SaveMoc3")).Click += delegate
-            {
-                connection.Mainform.OpenFolderDelegate(o =>
-                {
-                    Util.SaveMP(Util.MPType.GetMocByClassName | Util.MPType.GetMocByHeader, o.Folder, true);
+                    Live2DExtractor.SaveAllByAnimator(AssetList.ExportableAssets.Where(x => x.Type == ClassIDType.Animator), o.Folder, withPathIDCheck.Checked);
                 });
             };
 
-            this.SetCommand(new ToolStripMenuItem("GetL2DByAnimator")).Click += delegate
+            this.SetCommand(new ToolStripMenuItem("SaveMotionsBySelected") { ToolTipText = "Select target Animators&AnimationClips then click." }).Click += delegate
             {
-
                 connection.Mainform.OpenFolderDelegate(o =>
                 {
-                    Util.GetL2DByAnimator(o.Folder,false);
+                    Live2DExtractor.SaveMotionsBySelectedAnimatorClips(o.Folder, withPathIDCheck.Checked);
                 });
-                
             };
 
-            this.SetCommand(new ToolStripMenuItem("GetL2DByAnimator(motionNameWithIndex)")).Click += delegate
+            this.SetCommand(new ToolStripMenuItem("ForceSaveAsMoc3") { ToolTipText = "Select target MonoBehaviour then click." }).Click += delegate
             {
-
                 connection.Mainform.OpenFolderDelegate(o =>
                 {
-                    Util.GetL2DByAnimator(o.Folder, true);
+                    Live2DExtractor.ForceSaveAsMoc3(AssetList.GetSelectedAssets(), o.Folder, withPathIDCheck.Checked);
                 });
+            };
 
+            this.SetCommand(new ToolStripMenuItem("ForceSaveAsPhysics") { ToolTipText = "Select target MonoBehaviour then click." }).Click += delegate
+            {
+                connection.Mainform.OpenFolderDelegate(o =>
+                {
+                    Live2DExtractor.ForceSaveAsPhysics(AssetList.GetSelectedAssets(), o.Folder, withPathIDCheck.Checked);
+                });
             };
         }
     }
